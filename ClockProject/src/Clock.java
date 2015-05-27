e9d1eb76fefe9724edfe6a4083d929f7ce540d10
나은 <@Choi>
2015-05-27 17:24:43 +0900

    Add labels and bottons

--- ClockProject/src/Clock.java
+++ ClockProject/src/Clock.java
@@ -7,11 +7,26 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JTabbedPane;
 import javax.swing.JScrollPane;
 import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+
+import java.awt.FlowLayout;
 
 
 public class Clock extends JFrame {
 
 	private JPanel contentPane;
+	private JTabbedPane tabbedPane;
+	private JPanel Clock;
+	private JLabel lblTime;
+	private JLabel lblDate;
+	private JPanel panel;
+	private JButton btnChange;
+	private JPanel Alarm;
+	private JPanel panel_1;
+	private JScrollPane scrollPane;
+	private JButton btnAdd;
 
 	/**
 	 * Launch the application.
@@ -33,6 +48,7 @@ public class Clock extends JFrame {
 	 * Create the frame.
 	 */
 	public Clock() {
+		setTitle("Clock");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -40,18 +56,40 @@ public class Clock extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		contentPane.add(tabbedPane, BorderLayout.CENTER);
 		
-		JPanel Clock = new JPanel();
+		Clock = new JPanel();
 		tabbedPane.addTab("Clock", null, Clock, null);
+		Clock.setLayout(new GridLayout(0, 1, 0, 0));
 		
-		JPanel Alarm = new JPanel();
+		lblTime = new JLabel("Time");
+		lblTime.setHorizontalAlignment(SwingConstants.CENTER);
+		Clock.add(lblTime);
+		
+		lblDate = new JLabel("Date");
+		lblDate.setHorizontalAlignment(SwingConstants.CENTER);
+		Clock.add(lblDate);
+		
+		panel = new JPanel();
+		Clock.add(panel);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		btnChange = new JButton("Change date and time");
+		panel.add(btnChange);
+		
+		Alarm = new JPanel();
 		tabbedPane.addTab("Alarm", null, Alarm, null);
-		Alarm.setLayout(new GridLayout(0, 1, 0, 0));
+		Alarm.setLayout(new BorderLayout(0, 0));
 		
-		JScrollPane scrollPane = new JScrollPane();
+		scrollPane = new JScrollPane();
 		Alarm.add(scrollPane);
+		
+		panel_1 = new JPanel();
+		scrollPane.add(panel_1);
+		
+		btnAdd = new JButton("Add");
+		Alarm.add(btnAdd, BorderLayout.SOUTH);
 	}
 
 }
